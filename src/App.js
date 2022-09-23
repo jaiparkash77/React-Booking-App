338f959e2b60d8d58dc830036033b9a85abdbd1a
Jai Parkash <parkashj05@gmail.com>
2022-09-23 17:07:35 +0530

    deployment fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Hotel } from "./pages/hotel/Hotel";
 import { List } from "./pages/list/List";
